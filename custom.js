e731f8c0a1abd20e4753ed5860f4832ad81bd7a1
ukvaddi <ukvaddi@gmail.com>
2020-09-18 17:49:12 +0530

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,5 +1,6 @@
 Shopify.onCartUpdate = function(cart) {
   alert("There are now "+ cart.item_count + " items in the cart."); 
   alert("hi");   
+  alert("hi..this is shopify");
 }
-alert("hi..this is shopify");
+
